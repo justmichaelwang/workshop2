1508a1e5366d008efa5d71e37eabf1900fbf46ef
Yifan Ngok <jsjm711@gmail.com>
2017-03-13 15:34:48 +1100

    Elbert 2nd

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,7 +1,18 @@
+import java.util.Date;
 
 public class Assignment {
 	public String name;
 	public String description;
 	public Date dueDate;
+	public int maxAttempts;
 	
+	public Submission[] validSubmissions(){
+		return null;
+		
+	}
+	
+	public Submission[] invalidSubmissions(){
+		return null;
+		
+	}
 }
