d469ecac467ce40714180562303bc762d94c25b4
JJJJessie <jjjjessie@Siqis-MacBook-Pro.local>
2017-03-13 16:54:46 +1100

    Jessie3

--- src/Student.java
+++ src/Student.java
@@ -3,6 +3,7 @@ public abstract class Student extends Person {
 	public String studentID;
 	public String email;
 	
+	
 	public void sendEmailMsg (String msg) {
 		
 	}
--- src/Submission.java
+++ src/Submission.java
@@ -1,13 +1,23 @@
 // Sample private submission class to be replaced by your implementation
-class Submission {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Submission {
     public int attemptNum;
     public int numFiles;
     private File[] files;
     // Return two fake files
-    public File[] includedFiles(){
+    public File[] includedFiles() throws FileNotFoundException{
         File[] files = new File[2];
-        files[0] = new File();
-        files[1] = new File();
+        files[0] = new File("test1.txt");
+        files[1] = new File("test2.txt");
+        String astring, bstring;
+        Scanner fileIn1 = new Scanner(files[0]);
+        Scanner fileIn2 = new Scanner(files[1]);
+        astring = fileIn1.nextLine();
+        System.out.println(astring);
         return files;
     }
 }
