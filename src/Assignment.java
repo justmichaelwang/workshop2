55ef566123bce786a4cf7995341b28958058613a
Yifan Ngok <jsjm711@gmail.com>
2017-03-13 15:16:15 +1100

    Elbert 1st step

---
+++ src/Assignment.java
@@ -0,0 +1,7 @@
+
+public class Assignment {
+	public String name;
+	public String description;
+	public Date dueDate;
+	
+}
---
+++ src/Subject.java
@@ -0,0 +1,19 @@
+
+public class Subject {
+	public String name;
+	public String subjectCode;
+	
+	public Subject(String name, String subjectCode){
+		this.name = name;
+		this.subjectCode = subjectCode;
+	}
+	
+	public void createAssignment(){
+		
+	}
+	
+	public void deleteAssignment(String name){
+		
+	}
+	
+}
